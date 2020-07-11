e46938d030ab3fd1ca1c17473519d653b25a981d
Steven <3180101968@zju.edu.cn>
2020-07-11 10:21:46 +0800

    Add Homework5

---
+++ Homework5_Network/dou/src/main/java/com/bytedance/androidcamp/network/dou/MainActivity.java
@@ -0,0 +1,336 @@
+package com.bytedance.androidcamp.network.dou;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.bytedance.androidcamp.network.dou.api.IMiniDouyinService;
+import com.bytedance.androidcamp.network.dou.model.GetVideosResponse;
+import com.bytedance.androidcamp.network.dou.model.PostVideoResponse;
+import com.bytedance.androidcamp.network.dou.model.Video;
+import com.bytedance.androidcamp.network.dou.util.ResourceUtils;
+import com.bytedance.androidcamp.network.lib.util.ImageHelper;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+    private int STORAGE_PERMISSION_CODE = 1;
+    private static final int PICK_IMAGE = 1;
+    private static final int PICK_VIDEO = 2;
+    private static final String TAG = "MainActivity";
+    private RecyclerView mRv;
+    private List<Video> mVideos = new ArrayList<>();
+    public Uri mSelectedImage;
+    private Uri mSelectedVideo;
+    public Button mBtn;
+    private Button mBtnRefresh;
+    private Button mBtnMineOnly;
+    private boolean mMineOnly = false;
+    private String mMyStudentID = "18888913725";
+    private String mMyUsername = "yanwenchen";
+
+    private Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(IMiniDouyinService.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+    private IMiniDouyinService miniDouyinService = retrofit.create(IMiniDouyinService.class);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initRecyclerView();
+        initBtns();
+    }
+
+    private void initBtns() {
+        mBtn = findViewById(R.id.btn);
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = mBtn.getText().toString();
+                if (getString(R.string.select_an_image).equals(s)) {
+                    //@TODO 1填充选择图片的功能
+                    try{
+                        chooseImage();
+                    } catch (Exception e) {
+                        Log.d(TAG, e.getMessage());
+                        Toast.makeText(MainActivity.this, "Fail at choosing image!", Toast.LENGTH_SHORT).show();
+                    }
+
+                } else if (getString(R.string.select_a_video).equals(s)) {
+                    //@TODO 2填充选择视频的功能
+                    try{
+                        chooseVideo();
+                    } catch (Exception e) {
+                        Log.d(TAG, e.getMessage());
+                        Toast.makeText(MainActivity.this, "Fail at choosing video!", Toast.LENGTH_SHORT).show();
+                    }
+                } else if (getString(R.string.post_it).equals(s)) {
+                    if (mSelectedVideo != null && mSelectedImage != null) {
+                        //@TODO 3调用上传功能
+                        try{
+                            /**
+                             * from: https://codinginflow.com/tutorials/android/run-time-permission-request
+                             */
+                            if (ContextCompat.checkSelfPermission(MainActivity.this,
+                                    Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                                postVideo();
+                            } else {
+                                requestStoragePermission();
+                            }
+                        } catch (Exception e) {
+                            Log.d(TAG, e.getMessage());
+                            Toast.makeText(MainActivity.this, "Fail at posting!", Toast.LENGTH_SHORT).show();
+                        }
+
+                    } else {
+                        throw new IllegalArgumentException("error data uri, mSelectedVideo = "
+                                + mSelectedVideo
+                                + ", mSelectedImage = "
+                                + mSelectedImage);
+                    }
+                } else if ((getString(R.string.success_try_refresh).equals(s))) {
+                    mBtn.setText(R.string.select_an_image);
+                }
+            }
+        });
+
+        mBtnRefresh = findViewById(R.id.btn_refresh);
+        mBtnMineOnly = findViewById(R.id.btn_my);
+        mBtnMineOnly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mMineOnly) {
+                    mMineOnly = false;
+                    mBtnMineOnly.setText(R.string.my);
+                }
+                else {
+                    mMineOnly = true;
+                    mBtnMineOnly.setText(R.string.all);
+                }
+                fetchFeed(mRv);
+            }
+        });
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public ImageView img;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            img = itemView.findViewById(R.id.img);
+        }
+
+        public void bind(final Activity activity, final Video video) {
+            ImageHelper.displayWebImage(video.imageUrl, img);
+            img.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    VideoActivity.launch(activity, video.videoUrl);
+                }
+            });
+        }
+    }
+
+    private void initRecyclerView() {
+        mRv = findViewById(R.id.rv);
+        mRv.setLayoutManager(new LinearLayoutManager(this));
+        mRv.setAdapter(new RecyclerView.Adapter<MyViewHolder>() {
+            @NonNull
+            @Override
+            public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                return new MyViewHolder(
+                        LayoutInflater.from(MainActivity.this)
+                                .inflate(R.layout.video_item_view, viewGroup, false));
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull MyViewHolder viewHolder, int i) {
+                final Video video = mVideos.get(i);
+                viewHolder.bind(MainActivity.this, video);
+            }
+
+            @Override
+            public int getItemCount() {
+                return mVideos.size();
+            }
+        });
+    }
+
+    public void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"),
+                PICK_IMAGE);
+    }
+
+    public void chooseVideo() {
+        Intent intent = new Intent();
+        intent.setType("video/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Video"), PICK_VIDEO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.d(TAG, "onActivityResult() called with: requestCode = ["
+                + requestCode
+                + "], resultCode = ["
+                + resultCode
+                + "], data = ["
+                + data
+                + "]");
+
+        if (resultCode == RESULT_OK && null != data) {
+            if (requestCode == PICK_IMAGE) {
+                mSelectedImage = data.getData();
+                Log.d(TAG, "selectedImage = " + mSelectedImage);
+                mBtn.setText(R.string.select_a_video);
+            } else if (requestCode == PICK_VIDEO) {
+                mSelectedVideo = data.getData();
+                Log.d(TAG, "mSelectedVideo = " + mSelectedVideo);
+                mBtn.setText(R.string.post_it);
+            }
+        }
+    }
+
+    private MultipartBody.Part getMultipartFromUri(String name, Uri uri) {
+        File f = new File(ResourceUtils.getRealPath(MainActivity.this, uri));
+        RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), f);
+        return MultipartBody.Part.createFormData(name, f.getName(), requestFile);
+    }
+
+    private void postVideo() {
+        mBtn.setText("POSTING...");
+        mBtn.setEnabled(false);
+        MultipartBody.Part coverImagePart = getMultipartFromUri("cover_image", mSelectedImage);
+        MultipartBody.Part videoPart = getMultipartFromUri("video", mSelectedVideo);
+        //@TODO 4下面的id和名字替换成自己的
+        miniDouyinService.postVideo( mMyStudentID, mMyUsername, coverImagePart, videoPart).enqueue(
+                new Callback<PostVideoResponse>() {
+                    @Override
+                    public void onResponse(Call<PostVideoResponse> call, Response<PostVideoResponse> response) {
+                        if (response.body() != null) {
+                            Toast.makeText(MainActivity.this, response.body().toString(), Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                        mBtn.setText(R.string.select_an_image);
+                        mBtn.setEnabled(true);
+                    }
+
+                    @Override
+                    public void onFailure(Call<PostVideoResponse> call, Throwable throwable) {
+                        mBtn.setText(R.string.select_an_image);
+                        mBtn.setEnabled(true);
+                        Toast.makeText(MainActivity.this, throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    public void fetchFeed(View view) {
+        mBtnRefresh.setText("requesting...");
+        mBtnRefresh.setEnabled(false);
+        miniDouyinService.getVideos().enqueue(new Callback<GetVideosResponse>() {
+            @Override
+            public void onResponse(Call<GetVideosResponse> call, Response<GetVideosResponse> response) {
+                if (response.body() != null && response.body().videos != null) {
+                    //@TODO  5服务端没有做去重，拿到列表后，可以在端侧根据自己的id，做列表筛选。
+                    if(mMineOnly) {
+                        mVideos.clear();
+                        for(Video v : response.body().videos) {
+                            if(v.userName.equals(mMyUsername)
+                                    && v.studentId.equals((mMyStudentID))){
+                                mVideos.add(v);
+                            }
+                        }
+                    }
+                    else {
+                        mVideos = response.body().videos;
+                    }
+                    mRv.getAdapter().notifyDataSetChanged();
+                }
+                mBtnRefresh.setText(R.string.refresh_feed);
+                mBtnRefresh.setEnabled(true);
+
+            }
+
+            @Override
+            public void onFailure(Call<GetVideosResponse> call, Throwable throwable) {
+                mBtnRefresh.setText(R.string.refresh_feed);
+                mBtnRefresh.setEnabled(true);
+                Toast.makeText(MainActivity.this, throwable.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /**
+     *  from: https://codinginflow.com/tutorials/android/run-time-permission-request
+     */
+    private void requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is needed because of this and that")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == STORAGE_PERMISSION_CODE)  {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
