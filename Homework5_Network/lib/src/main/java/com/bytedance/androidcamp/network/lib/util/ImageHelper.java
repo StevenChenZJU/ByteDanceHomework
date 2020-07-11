cad69b8058403022f5697abb64ee229ecdbd2620
Steven <3180101968@zju.edu.cn>
2020-07-11 22:25:37 +0800

    Add loading image

--- Homework5_Network/lib/src/main/java/com/bytedance/androidcamp/network/lib/util/ImageHelper.java
+++ Homework5_Network/lib/src/main/java/com/bytedance/androidcamp/network/lib/util/ImageHelper.java
@@ -2,9 +2,10 @@ package com.bytedance.androidcamp.network.lib.util;
 
 import android.widget.ImageView;
 import com.bumptech.glide.Glide;
+import com.bytedance.androidcamp.network.lib.R;
 
 public class ImageHelper {
     public static void displayWebImage(String url, ImageView imageView) {
-        Glide.with(imageView.getContext()).load(url).into(imageView);
+        Glide.with(imageView.getContext()).load(url).placeholder(R.mipmap.loading_image).into(imageView);
     }
 }
