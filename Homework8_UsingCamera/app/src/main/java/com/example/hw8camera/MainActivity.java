45e4e11b6c9efcde3fa976a30b9e46733db77bb0
Steven <3180101968@zju.edu.cn>
2020-07-16 09:53:01 +0800

    Add Homework8

--- Homework8_UsingCamera/app/src/main/java/com/example/hw8camera/MainActivity.java
+++ Homework8_UsingCamera/app/src/main/java/com/example/hw8camera/MainActivity.java
@@ -1,14 +1,205 @@
 package com.example.hw8camera;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
+import androidx.core.graphics.PathUtils;
 
+import android.Manifest;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.VideoView;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private ImageView mImageView;
+    private Button mButton;
+    private static final int REQUEST_CODE_RECORD_PATH = 1;
+    private static final int CAMERA_PERMISSION_CODE = 2;
+    private Camera mCamera;
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mHolder;
+    private MediaRecorder mMediaRecorder;
+    private VideoView mVideoView;
+    private String mp4Path;
+    private boolean isRecording = false;
+
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mVideoView = findViewById(R.id.vv_video);
+        mButton = findViewById(R.id.bt_video);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isRecording) {
+                    mButton.setText(R.string.record);
+                    mMediaRecorder.stop();
+                    mMediaRecorder.reset();
+                    mMediaRecorder.release();
+                    mMediaRecorder = null;
+                    mCamera.lock();
+
+                    mVideoView.setVisibility(View.VISIBLE);
+                    mVideoView.setVideoPath(mp4Path);
+                    mVideoView.start();
+                }
+                else {
+                    if(prepareVideoRecorder()) {
+                        mButton.setText(R.string.stop);
+                        mMediaRecorder.start();
+                    }
+                }
+                isRecording = !isRecording;
+            }
+        });
+
+        mSurfaceView = findViewById(R.id.sv_video);
+        mHolder = mSurfaceView.getHolder();
+        mHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder holder) {
+                try {
+                    mCamera.setPreviewDisplay(holder);
+                    mCamera.startPreview();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+                if(holder.getSurface() != null) {
+                    mCamera.stopPreview();
+                    try {
+                        mCamera.setPreviewDisplay(holder);
+                        mCamera.startPreview();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+                mCamera.stopPreview();
+                mCamera.release();
+                mCamera = null;
+            }
+        });
     }
+
+    private String getOutputMediaPath() {
+        File mediaStorageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+                .format(new Date());
+        File mediaFile = new File(mediaStorageDir, "VID_" + timeStamp + ".mp4");
+        if(!mediaFile.exists()) {
+            mediaFile.getParentFile().mkdirs();
+        }
+        return mediaFile.getAbsolutePath();
+    }
+    
+    private void initCamera() {
+        try {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[] {Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO}, CAMERA_PERMISSION_CODE);
+            mCamera = Camera.open();
+            setCameraDisplayOrientation(this, 0, mCamera );
+        } catch (Exception e) {
+            Log.d(TAG, "Error when opening");
+            e.printStackTrace();
+        }
+    }
+    private static void setCameraDisplayOrientation(Activity activity,
+                                                   int cameraId, android.hardware.Camera camera) {
+        android.hardware.Camera.CameraInfo info =
+                new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = activity.getWindowManager().getDefaultDisplay()
+                .getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        camera.setDisplayOrientation(result);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mCamera == null) {
+            initCamera();
+        }
+        mCamera.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mCamera.stopPreview();
+    }
+
+    private boolean  prepareVideoRecorder() {
+        mMediaRecorder = new MediaRecorder();
+        mCamera.unlock();
+        mMediaRecorder.setCamera(mCamera);
+        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+
+        mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+        mp4Path = getOutputMediaPath();
+        mMediaRecorder.setOutputFile(mp4Path);
+
+        mMediaRecorder.setPreviewDisplay(mHolder.getSurface());
+        mMediaRecorder.setOrientationHint(90);
+
+        try {
+            mMediaRecorder.prepare();
+        } catch (Exception e) {
+            mMediaRecorder.reset();
+            mMediaRecorder.release();
+            return false;
+        }
+        return true;
+    }
+
 }
